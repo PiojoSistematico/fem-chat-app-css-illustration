3f0f995b1049ccfdfed12913de8438b589af8f05
Jorge Saa <piojo.sistematico@gmail.com>
2023-10-13 21:23:32 -0300

    basic html markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import avatar from "./assets/images/avatar.jpg";
+import dog1 from "./assets/images/dog-image-1.jpg";
+import dog2 from "./assets/images/dog-image-2.jpg";
+import dog3 from "./assets/images/dog-image-3.jpg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="align-center flex justify-center border-b-2 bg-gray-900 font-bold">
+        <picture></picture>
+        <section title="phone">
+          <div className="screen">
+            <div className="header">
+              <div>{"<"}</div>
+              <div className="profile">
+                <picture>
+                  <img src={avatar} alt="avatar" />
+                </picture>
+                <div>
+                  <span>Samuel Green</span>
+                  <span>Available to Walk</span>
+                </div>
+                <div>three dots</div>
+              </div>
+            </div>
+            <div className="chat">
+              <p>That sounds great. I’d be happy with that.</p>
+              <p>Could you send over some pictures of your dog, please?</p>
+              <div>
+                <picture>
+                  <img src={dog1} alt="dog photo" />
+                </picture>
+                <picture>
+                  <img src={dog2} alt="dog photo" />
+                </picture>
+                <picture>
+                  <img src={dog3} alt="dog photo" />
+                </picture>
+              </div>
+              <p>Here are a few pictures. She’s a happy girl!</p>
+              <p>Can you make it?</p>
+              <p>
+                She looks so happy! The time we discussed works. How long shall
+                I take her out for?
+              </p>
+              <div>
+                <div>Cirle</div> 30 minute walk <span>$29</span>
+              </div>
+              <div>
+                <div>Cirle</div> 1 hour walk <span>$49</span>
+              </div>
+              <div>
+                <div>Input</div>
+                <div>Button</div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section title="description">
+          <h1>Simple booking</h1>
+          <p>
+            Stay in touch with our dog walkers through the chat interface. This
+            makes it easy to discuss arrangements and make bookings. Once the
+            walk has been completed you can rate your walker and book again all
+            through the chat.
+          </p>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
