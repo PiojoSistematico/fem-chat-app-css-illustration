e767fec9eddc99547e1da7a930a387484001bb62
Jorge Saa <piojo.sistematico@gmail.com>
2024-04-04 19:56:17 -0300

    functional app

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,18 @@ function App() {
 
   return (
     <>
-      <main className="font-regular relative flex flex-col items-center justify-items-center  gap-10 bg-neutral-2 font-rubik text-base text-primary-6">
+      <main className="font-regular relative flex h-screen flex-col items-center gap-4 bg-neutral-2 py-8 font-rubik text-sm text-primary-6 md:grid md:grid-cols-2 md:gap-32">
+        <div className="absolute left-0 top-0 z-0 h-screen w-screen -translate-x-1/2 -translate-y-1/2 rounded-b-[400px] bg-gradient-to-b from-gradient-1 to-gradient-2 md:w-5/12 md:-translate-x-1/4 md:-translate-y-1/4"></div>
         <section
           title="phone"
-          className=" w-[70%] rounded-3xl border-2 border-red-400 bg-neutral-1 p-2 text-sm"
+          className="z-10 w-80 rounded-3xl bg-neutral-1 p-3 text-xs md:justify-self-end"
         >
           <div title="screen" className="rounded-2xl bg-neutral-2">
             <div
               title="header"
-              className="relative flex flex-row items-center justify-between rounded-t-xl bg-gradient-to-r from-gradient-2 to-gradient-1 p-2 text-neutral-1"
+              className="relative flex flex-row items-center justify-between rounded-t-xl bg-gradient-to-r from-gradient-2 to-gradient-1 p-2 pt-4 text-neutral-1"
             >
-              <div className="absolute h-4 w-8 rounded-md bg-neutral-1"></div>
+              <div className="absolute -top-3 left-1/2 h-6 w-32 -translate-x-1/2 rounded-lg bg-neutral-1"></div>
               <div className="flex items-center gap-2">
                 <div>{"<"}</div>
                 <picture>
@@ -30,8 +31,8 @@ function App() {
                   />
                 </picture>
                 <div className="flex flex-col">
-                  <span>Samuel Green</span>
-                  <span>Available to Walk</span>
+                  <span className="font-medium">Samuel Green</span>
+                  <span className="text-primary-1">Available to Walk</span>
                 </div>
               </div>
 
@@ -41,10 +42,14 @@ function App() {
                 <span>.</span>
               </div>
             </div>
-            <div className="chat">
-              <p>That sounds great. I’d be happy with that.</p>
-              <p>Could you send over some pictures of your dog, please?</p>
-              <div className="flex flex-row gap-2">
+            <div className="flex flex-col gap-2 p-4">
+              <p className="w-9/12 rounded-md bg-primary-1 p-2 text-primary-2">
+                That sounds great. I’d be happy with that.
+              </p>
+              <p className="w-9/12 rounded-md bg-primary-1 p-2 text-primary-2">
+                Could you send over some pictures of your dog, please?
+              </p>
+              <div className="flex flex-row gap-2 self-end">
                 <picture>
                   <img
                     src={dog1}
@@ -67,30 +72,44 @@ function App() {
                   />
                 </picture>
               </div>
-              <p>Here are a few pictures. She’s a happy girl!</p>
-              <p>Can you make it?</p>
-              <p>
+              <p className="w-9/12 self-end rounded-md bg-neutral-1 p-2">
+                Here are a few pictures. She’s a happy girl!
+              </p>
+              <p className="w-9/12 self-end rounded-md bg-neutral-1 p-2">
+                Can you make it?
+              </p>
+              <p className="w-9/12 rounded-md bg-primary-1 p-2 text-primary-2">
                 She looks so happy! The time we discussed works. How long shall
                 I take her out for?
               </p>
-              <div>
-                <div>Cirle</div> 30 minute walk <span>$29</span>
+              <div className="flex w-10/12 flex-row items-center justify-between rounded-md bg-gradient-to-r from-gradient-1 to-gradient-2 p-2 text-neutral-1">
+                <div className="flex flex-row items-center gap-2">
+                  <div className="h-4 w-4 rounded-full border border-solid border-neutral-1"></div>{" "}
+                  <span>30 minute walk</span>
+                </div>
+                <span className="font-bold">$29</span>
               </div>
-              <div>
-                <div>Cirle</div> 1 hour walk <span>$49</span>
+              <div className="flex w-10/12 flex-row items-center justify-between rounded-md bg-gradient-to-r from-gradient-1 to-gradient-2 p-2 text-neutral-1">
+                <div className="flex flex-row items-center gap-2">
+                  <div className="h-4 w-4 rounded-full border border-solid border-neutral-1"></div>{" "}
+                  <span>1 hour walk</span>
+                </div>
+                <span className="font-bold">$49</span>
               </div>
-              <div>
-                <div>Input</div>
-                <div>Button</div>
+              <div className="mt-8 flex flex-row items-center justify-between rounded-2xl bg-neutral-1 p-2">
+                <p>Type a message...</p>
+                <p className="flex h-6 w-6 flex-row items-center justify-center rounded-full bg-neutral-3 text-neutral-1">
+                  #
+                </p>
               </div>
             </div>
           </div>
         </section>
-        <section title="description" className="p-8 text-center">
+        <section title="description" className="p-8 text-center md:text-left">
           <h1 className=" pb-4 text-3xl font-medium text-primary-5">
             Simple booking
           </h1>
-          <p>
+          <p className="pr-8 text-base">
             Stay in touch with our dog walkers through the chat interface. This
             makes it easy to discuss arrangements and make bookings. Once the
             walk has been completed you can rate your walker and book again all
